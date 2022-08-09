41bea9dfc7e71e33c0f3c0404e4c69f3df554924
Rushikesh Patade <pataderushikesh@gmail.com>
2022-08-10 05:04:57 +0530

    fetch data

--- lib/services/AadharOTP/otp/auth/otp_auth.dart
+++ lib/services/AadharOTP/otp/auth/otp_auth.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'otp_auth_util.dart';
 
 Future<Map<String, dynamic>> aadharSignIn() async {
   Uri url = Uri.parse("https://aadharotp.rushour0.repl.co/digilocker/initiate");
   http.Response response = await http.get(url);
   Map<String, dynamic> data = jsonDecode(response.body);
-  Map<String, dynamic> result = {
-    'sign_in_url': data['url'],
-    'request_id': data['id']
-  };
 
-  return result;
+  return data;
+}
+
+Future<Map<String, dynamic>> getAadharDocument(String requestId) async {
+  Uri url = Uri.parse(
+      "https://aadharotp.rushour0.repl.co/digilocker/fetch?request_id=$requestId");
+  http.Response response = await http.post(url);
+  Map<String, dynamic> data = jsonDecode(response.body);
+
+  return data;
 }
