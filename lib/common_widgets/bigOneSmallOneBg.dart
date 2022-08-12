b9148c440a31a700da4cbb3f97dca3eb09c7f655
Rushikesh Patade <pataderushikesh@gmail.com>
2022-08-12 17:54:33 +0530

    main page set

--- lib/common_widgets/bigOneSmallOneBg.dart
+++ lib/common_widgets/bigOneSmallOneBg.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class WTThree extends StatelessWidget {
-  const WTThree({Key? key}) : super(key: key);
+class BigOneSmallOneBG extends StatelessWidget {
+  const BigOneSmallOneBG({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
