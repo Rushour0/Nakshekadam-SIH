f50d604c323a8e5c21fdb191cbbdc98a91707b9e
tushar725mittal <tushar725mittal@gmail.com>
2022-08-21 15:41:35 +0530

    post login student

---
+++ lib/screens/drawer/drawer_main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nakshekadam/screens/drawer/components/drawer_listtile.dart';
+import 'package:nakshekadam/screens/drawer/components/drawer_user_info_card.dart';
+import 'package:readmore/readmore.dart';
+
+import 'package:nakshekadam/common_widgets/backgrounds/bigOneSmallOneBg.dart';
+import 'package:nakshekadam/common_widgets/formfields.dart';
+import 'package:nakshekadam/globals.dart';
+import 'package:nakshekadam/screens/main/tabs/counsellor_page/components/counsellor_card_dialog.dart';
+import 'package:nakshekadam/screens/student_post_login/info_collection/components/student_parent_card.dart';
+import 'package:nakshekadam/screens/student_post_login/info_collection/student_details_collection.dart';
+import 'package:nakshekadam/screens/student_post_login/student_main/tabs/counsellor_tabs/components/professional_counsellor_card.dart';
+import 'package:nakshekadam/screens/student_post_login/student_main/tabs/counsellor_tabs/explore_counsellors/components/explore_counsellor_cards.dart';
+import 'package:nakshekadam/screens/student_post_login/student_main/tabs/counsellor_tabs/explore_counsellors/components/explore_counsellor_sort.dart';
+
+class DrawerMain extends StatefulWidget {
+  const DrawerMain({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<DrawerMain> createState() => _DrawerMainState();
+}
+
+class _DrawerMainState extends State<DrawerMain> {
+  TextEditingController _searchController = TextEditingController();
+  List<String> drawerTabs = [
+    "My test results",
+    "FAQs",
+    "Send Feedback",
+    "Report Problems"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final List<double> tempDimensions = [
+      MediaQuery.of(context).size.width,
+      MediaQuery.of(context).size.height
+    ];
+    final double screenHeight = tempDimensions[0] > tempDimensions[1]
+        ? tempDimensions[0]
+        : tempDimensions[1];
+    final double screenWidth = tempDimensions[0] > tempDimensions[1]
+        ? tempDimensions[1]
+        : tempDimensions[0];
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        // leading: const SizedBox.shrink(),
+        toolbarHeight: screenHeight * 0.5,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        title: SizedBox(
+          width: screenWidth,
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ButtonStyle(
+                overlayColor: MaterialStateProperty.all(Colors.transparent),
+              ),
+              child: const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Icon(Icons.arrow_back_ios_new_rounded)),
+            ),
+            const DrawerUserInfoCard()
+          ]),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        height: screenHeight,
+        width: screenWidth,
+        child: Stack(
+          children: [
+            Positioned(
+              height: screenHeight,
+              width: screenWidth,
+              child: const BigOneSmallOneBG(),
+            ),
+            Positioned(
+              top: 0,
+              width: screenWidth,
+              height: screenHeight,
+              child: SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * 0.04,
+                      vertical: screenHeight * 0.007),
+                  child: NotificationListener<OverscrollIndicatorNotification>(
+                    onNotification: (overscroll) {
+                      overscroll.disallowIndicator();
+                      return true;
+                    },
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: screenHeight * 0.02),
+                            child: Column(
+                              children: drawerTabs
+                                  .map(
+                                    (drawerOption) => Padding(
+                                      padding: EdgeInsets.only(
+                                          bottom: screenHeight * 0.015),
+                                      child: Card(
+                                        elevation: 3,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(
+                                              screenWidth * 0.05),
+                                        ),
+                                        child: drawerListTile(
+                                          screenWidth: screenWidth,
+                                          title: drawerOption,
+                                          icon: (drawerOption ==
+                                                  "My test results")
+                                              ? Icons.equalizer_rounded
+                                              : (drawerOption == "FAQs")
+                                                  ? CupertinoIcons
+                                                      .question_square
+                                                  : (drawerOption ==
+                                                          "Send Feedback")
+                                                      ? Icons.feedback_outlined
+                                                      : CupertinoIcons
+                                                          .exclamationmark_octagon,
+                                          onTap: () {},
+                                          index:
+                                              drawerTabs.indexOf(drawerOption),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
